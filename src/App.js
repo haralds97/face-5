d1327b85a8103f4e9c58c6e8c3b7a647d2bb8dda
haraldsDev <Haralds.Dev@gmail.com>
2018-09-18 22:28:25 +0300

    upgrade SignIn and Register -r eve of 18sep

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Rank from './components/Rank/Rank';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+import Clarifai from 'clarifai'; 
 
 const app = new Clarifai.App({ 
  apiKey: '66c26976e675482eaa843e8fc6b634ca'
