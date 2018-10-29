c3b7867e9df220c974441194320156eacdd332c9
haraldsDev <Haralds.Dev@gmail.com>
2018-10-29 11:59:56 +0200

    situation after 29okt session - clarifai key moved to backend, but smth not working in face detection

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -29,9 +29,9 @@ class SignIn extends Component {
 		.then(response => response.json())
 		.then(user => {
 			console.log(user);
-			if (user) {
+			if (user.id) {
 				this.props.loadUser(user);
-				this.props.onRouteChange('home');
+				this.props.onRouteChange('home'); 
 			}
 		})
 	}
